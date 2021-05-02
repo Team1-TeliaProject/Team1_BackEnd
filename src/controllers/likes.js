b0d0306eb75bd1a645b66780a1d837ef1bc1f35d
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-05-02 23:17:18 +0300

    [Chores] cleanup

--- src/controllers/likes.js
+++
@@ -1,7 +0,0 @@
-const Company = require('../modals/company');
-const Talent = require('../modals/talent');
-
-const normalLike = async (req, res) => {};
-const superLike = async (req, res) => {};
-
-module.exports = { normalLike, superLike };
--- src/routes/likesRouter.js
+++
@@ -1,8 +0,0 @@
-const likesRouter = require('express').Router();
-
-const { normalLike, superLike } = require('../controllers/likes');
-
-likesRouter.use('/normal', normalLike);
-likesRouter.use('/super', superLike);
-
-module.exports = likesRouter;
